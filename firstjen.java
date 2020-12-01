7d4c6e364da175fc2b565a940866e14701c8dde3
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-12-01 03:29:08 -0800

    version 7

--- firstjen.java
+++ firstjen.java
@@ -3,10 +3,10 @@ class firstjen
 public static void main (String args[])
 {
 int a=70,b=40,c;
-c=a+b;
+c=a*b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result add of two number is :" +c);
+System.out.println ("The result multiply of two number is :" +c);
 
 }
 
