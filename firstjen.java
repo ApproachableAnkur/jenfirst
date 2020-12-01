55e5737d4a02396e8997b898a95c45eeeb9925e9
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-11-30 21:59:48 -0800

    version 1

---
+++ firstjen.java
@@ -0,0 +1,13 @@
+class firstjen
+{
+public static void main (String args[])
+{
+int a=70,b=40,c;
+c=a-b;
+System.out.println ("The first number is :"+a);
+System.out.println ("The second number is :"+b);
+System.out.println ("The result subtraction two number is :" +c);
+
+}
+
+}
